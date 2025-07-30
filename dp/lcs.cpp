f5adc8f505feed62abcfd329831b27317bc0ca2c
Efaz Ahmed <efazahmed.com@gmail.com>
2025-07-31 00:22:50 +0600

    Update lcs.cpp

--- dp/lcs.cpp
+++ dp/lcs.cpp
@@ -1,43 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int lcs(string a, string b)
+string lcs(string a, string b)
 {
     int n = a.size();
     int m = b.size();
-    int dp[n+1][m+1];
-    for(int i = 0 ; i < n; i++)
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    // Fill DP table
+    for(int i = 1; i <= n; i++)
     {
-        dp[i][0] = 0;
-    }
-    
-    for(int i = 0; i < m; i++) dp[0][i] = 0;
-    
-    for(int i = 1; i < n; i++)
-    {
-        for(int j = 1; j < m; j++)
+        for(int j = 1; j <= m; j++)
         {
-            //if_same hole len 1 bare r size decrese by 1:
             if(a[i-1] == b[j-1])
-            {
                 dp[i][j] = dp[i-1][j-1] + 1;
-            }
-            //same na hole
             else 
-            {
-                //case1:
-                int ans1 = dp[i-1][j];
-                //case2:
-                int ans2 = dp[i][j-1];
-                dp[i][j] = max(ans1,ans2);
-            }
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
-    return dp[n][m];
+
+    // Trace back to get the LCS string
+    int i = n, j = m;
+    string lcs = "";
+    while(i > 0 && j > 0)
+    {
+        if(a[i-1] == b[j-1])
+        {
+            lcs += a[i-1];
+            i--;
+            j--;
+        }
+        else if(dp[i-1][j] > dp[i][j-1])
+            i--;
+        else
+            j--;
+    }
+
+    reverse(lcs.begin(), lcs.end()); // since we built it backwards
+    return lcs;
 }
 
 int main()
 {
-    string a,b; cin >> a >> b;
-    cout << lcs(a,b) << endl;
+    string a, b;
+    cin >> a >> b;
+    string lcs = lcs(a, b);
+    cout << "LCS: " << lcs << endl;
+    cout << "Length: " << lcs.size() << endl;
 }
